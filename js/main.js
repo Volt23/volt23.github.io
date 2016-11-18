1c6aaffb2c8a0732b34cf4a8811d1aca3bebe48c
Volt23 <ernesto.volt@me.com>
2016-11-18 14:59:30 -0800

    huge upgrade

---
+++ js/main.js
@@ -0,0 +1,32 @@
+
+$(document).ready(function(){
+    var $menu = $('.menu');
+    $menu.click(function(){
+        $('.menuscreen').fadeToggle();
+        $('.logo2').fadeToggle();
+    });
+})
+
+$(window).load(function(){
+    var $container = $('.portfolioContainer');
+    $container.isotope({
+        itemSelector: '.grid-item',
+        percentPosition: true,
+        masonry: {
+        columnWidth: '.masonry__column',
+        gutter: '.masonry__gutter',
+        }
+    });
+
+    $('.portfolioFilter a').click(function(){
+        $('.portfolioFilter .current').removeClass('current');
+        $(this).addClass('current');
+        var selector = $(this).attr('data-filter');
+        $container.isotope({
+            filter: selector,
+         });
+         return false;
+    }); 
+
+});
+
